7d34f7fa0322fae4ea2efe28c20040d3c0190854
hj <2413302357@qq.com>
2024-02-28 09:55:10 +0800

    Add: unittest for Field

--- test.py
+++ test.py
@@ -26,6 +26,33 @@ class TestField(unittest.TestCase):
         result2 = f2._cov_null()
         self.assertEqual(result2, '')
 
+    def test_cov_auto_increment(self):
+        f = ss.Field('name', ss.Field.TYPE.VARCHAR, auto_increment=True)
+        result = f._cov_auto_increment()
+        self.assertEqual(result, 'AUTOINCREMENT')
+        f2 = ss.Field('name', ss.Field.TYPE.VARCHAR)
+        result2 = f2._cov_auto_increment()
+        self.assertEqual(result2, '')
+
+    def test_cov_pk(self):
+        f = ss.Field('name', ss.Field.TYPE.VARCHAR, pk=True)
+        result = f._cov_pk()
+        self.assertEqual(result, 'PRIMARY KEY')
+        f2 = ss.Field('name', ss.Field.TYPE.VARCHAR)
+        result2 = f2._cov_pk()
+        self.assertEqual(result2, '')
+
+    def test_dump(self):
+        f = ss.Field('name', ss.Field.TYPE.VARCHAR)
+        self.assertEqual(f.dump(),
+                         "name VARCHAR NOT NULL")
+        f2 = ss.Field('name', ss.Field.TYPE.TEXT, pk=True)
+        self.assertEqual(f2.dump(),
+                         "name TEXT PRIMARY KEY NOT NULL")
+        f3 = ss.Field('name', ss.Field.TYPE.TEXT, auto_increment=True)
+        self.assertEqual(f3.dump(),
+                         "name TEXT PRIMARY KEY AUTOINCREMENT")
+
 
 if __name__ == '__main__':
     unittest.main()
